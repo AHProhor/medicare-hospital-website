7c93a785219e7b79a5c6a10bb16e11ed581e93ce
PROHOR <ahprohor88@gmail.com>
2021-10-21 17:00:53 +0600

    read me file added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,14 +5,20 @@ import './Login.css';
 
 const Login = () => {
     const {user,signInUsingGoogle} = useFirebase();
+
+
+    const handelRegistration = e =>{
+        console.log('hsjdksjd');
+        e.preventDefault()
+    }
     return (
         <div className="loginStyle">
             <div>
             <h3>Login</h3>
-            <form onSubmit="">
+            <form onSubmit={handelRegistration}>
                 <input type="email" name="" id="" placeholder="Your Email"/>
                 <br />
-                <input type="possword" name="" id="" placeholder="Your Password"/>
+                <input type="possword" name="password" id="" placeholder="Your Password"/>
                 <br />
                 <input className="bg-primary text-light" type="submit" name="" value="Submit"/>
             </form>
